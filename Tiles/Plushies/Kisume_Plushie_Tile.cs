63ed7bb73b0089c3aa3820a2120bdf70861e20a6
Rhoenicx <rhoenicx@gmail.com>
2021-12-19 01:18:38 +0100

    Added Kisume Tile Top Anchors

--- Tiles/Plushies/Kisume_Plushie_Tile.cs
+++ Tiles/Plushies/Kisume_Plushie_Tile.cs
@@ -30,10 +30,12 @@ namespace Kourindou.Tiles.Plushies
             Main.tileSolid[Type] = false;
             // Prevent destroying this tile when hit
             Main.tileCut[Type] = false;
+
             // Tile Style
             TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
             // Tile Height
             TileObjectData.newTile.Height = 2;
+            // Tile Width
             TileObjectData.newTile.Width = 2;
             // Tile Size
             TileObjectData.newTile.CoordinateHeights = new int[]{ 16, 16 };
@@ -42,12 +44,19 @@ namespace Kourindou.Tiles.Plushies
             // Tile Anchors
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidWithTop | AnchorType.SolidTile | AnchorType.Table, TileObjectData.newTile.Width, 0);
 
+            // Alternate version that can hang from solid blocks
             TileObjectData.newAlternate.CopyFrom(TileObjectData.Style1x2Top);
+            // Tile Height
             TileObjectData.newAlternate.Height = 2;
+            // Tile Width
             TileObjectData.newAlternate.Width = 2;
+            // Tile Size
             TileObjectData.newAlternate.CoordinateHeights = new int[]{ 16, 16 };
+            // Tile origin on mouse pointer
             TileObjectData.newAlternate.Origin = new Point16(0, 1);
+            // Tile Anchors
             TileObjectData.newAlternate.AnchorTop = new AnchorData(AnchorType.SolidBottom | AnchorType.SolidSide | AnchorType.SolidTile, TileObjectData.newAlternate.Width, 0);
+            // Add Alternate Tile
             TileObjectData.addAlternate(0);
 
             // Add tile
