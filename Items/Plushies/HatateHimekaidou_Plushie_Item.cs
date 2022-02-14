96e4e05c6685f8cb3184e6e319c3f2a790184b62
Rhoenicx <rhoenicx@gmail.com>
2022-02-14 20:40:48 +0100

    Added Hatate Plushie

---
+++ Items/Plushies/HatateHimekaidou_Plushie_Item.cs
@@ -0,0 +1,80 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Kourindou.Tiles.Plushies;
+using Kourindou.Projectiles.Plushies;
+using Kourindou.Items.CraftingMaterials;
+using Kourindou.Tiles.Furniture;
+
+namespace Kourindou.Items.Plushies
+{
+    public class HatateHimekaidou_Plushie_Item : PlushieItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hatate Himekaidou Plushie");
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            // Information
+            item.value = Item.buyPrice(0, 5, 0, 0);
+            item.rare = ItemRarityID.Purple;
+
+            // Hitbox
+            item.width = 32;
+            item.height = 32;
+
+            // Usage and Animation
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTime = 15;
+            item.useAnimation = 15;
+            item.autoReuse = true;
+            item.useTurn = true;
+
+            // Tile placement fields
+            item.consumable = true;
+            item.createTile = TileType<HatateHimekaidou_Plushie_Tile>();
+
+            item.shootSpeed = 8f;
+
+            // Register as accessory, can only be equipped when plushie power mode setting is 2
+            item.accessory = true;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                shootSpeed = 8f;
+                projectileType = ProjectileType<HatateHimekaidou_Plushie_Projectile>();
+            }
+            return base.UseItem(player);
+        }
+
+        // This only executes when plushie power mode is 2
+        public override void PlushieEquipEffects(Player player)
+        {
+
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<BlackFabric>(), 2);
+			recipe.AddIngredient(ItemType<PinkFabric>(), 1);
+            recipe.AddIngredient(ItemType<PurpleFabric>(), 2);
+            recipe.AddIngredient(ItemID.Silk, 2);
+            recipe.AddIngredient(ItemID.BlackThread, 2);
+            recipe.AddIngredient(ItemID.PinkThread, 1);
+            recipe.AddIngredient(ItemType<PurpleThread>(), 2);
+            recipe.AddIngredient(ItemType<WhiteThread>(), 1);
+            recipe.AddRecipeGroup("Kourindou:Stuffing", 5);
+            recipe.AddTile(TileType<SewingMachine_Tile>());
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
